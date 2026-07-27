0ec8f772ac108c7bfaf393c761ba98a63341f547
Deivis-M <davidminango2009@gmail.com>
2026-07-26 23:17:02 -0500

    mostrar serpiente con funciones modificadas

--- serpiente.js
+++ serpiente.js
@@ -3,7 +3,21 @@
     const canvas = document.getElementById("canvasJuego");
     const ctx = canvas.getContext("2d");
     const TAMANIO_CELDA = 25;
-
+    const serpiente = [
+      {x:20,y:9},
+      {x:20,y:10},
+      {x:20,y:11},
+      {x:20,y:12},
+      {x:20,y:13},
+      {x:19,y:13},
+      {x:18,y:13},
+      {x:18,y:14},
+      {x:17,y:14},
+      {x:16,y:14},
+      {x:16,y:13},
+      {x:16,y:12},
+      {x:16,y:11},
+    ];
     
 
     // Primera pintura del juego al cargar la página
@@ -31,10 +45,31 @@
     function pintarParte(lineaX, lineaY){
       let x = lineaX * TAMANIO_CELDA
       let y = lineaY * TAMANIO_CELDA
-      ctx.fillStyle = "#ff0202";
+      ctx.fillStyle = "#2b9b8c ";
       ctx.fillRect(x,y,TAMANIO_CELDA,TAMANIO_CELDA);
-      ctx.strokeStyle = "#fdfdfd";
+      ctx.strokeStyle = "#ffffff";
       ctx.strokeRect (x,y,TAMANIO_CELDA,TAMANIO_CELDA);
+    }
+
+    function pintarCabeza(lineaX, lineaY){
+      let x = lineaX * TAMANIO_CELDA
+      let y = lineaY * TAMANIO_CELDA
+      ctx.fillStyle = "#165df5";
+      ctx.fillRect(x,y,TAMANIO_CELDA,TAMANIO_CELDA);
+      ctx.strokeStyle = "#ffffff";
+      ctx.strokeRect (x,y,TAMANIO_CELDA,TAMANIO_CELDA);
+    }
+
+    function pintarSerpiente(){
+      for (let i = 0;i<serpiente.length;i ++){
+        let datos = serpiente[i]
+        if (i == 0){
+          pintarCabeza(datos.x,datos.y)
+        }
+        else{
+        pintarParte(datos.x,datos.y)
+        }
+      }
     }
     // =========================
 
@@ -45,12 +80,7 @@
     function dibujarTodo() {
       limpiarCanvas();
       dibujarTablero();
-      pintarParte(5,5);
-      pintarParte(10,2);
-      pintarParte(0,(canvas.height/TAMANIO_CELDA)-1);
-      pintarParte((canvas.width/TAMANIO_CELDA)-1,(canvas.height/TAMANIO_CELDA)-1);
-      pintarParte((canvas.width/TAMANIO_CELDA)-1,0);
-      pintarParte(0,3);
+      pintarSerpiente();
     }
 
 
